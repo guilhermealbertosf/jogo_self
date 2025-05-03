9e57f58a26b9b23ee9be50feba627c4d755bff3e
Guilherme Alberto <gmightasf@gmail.com>
2025-05-03 14:22:46 -0300

    update select

--- script/batalha.js
+++ script/batalha.js
@@ -1,53 +1,5 @@
-
-
-
-
-function selecionar(){
-    alert('selecionado')
-}
-
-
-function Batalha(){
-
-// Monstro seleconado
-    let nome1 = document.getElementById('nome1')
-    let elemento1 = document.getElementById('elemento1')
-    let vida1 = document.getElementById('vida1')
-    let dano1 = document.getElementById('dano1')
-    let energia1 = document.getElementById('energia1')
-    let imagem1 = document.getElementById('imagem1')
-
-    nome1.innerText = `${monstro1.nome}`
-    elemento1.innerText = `Elemento: ${monstro1.elemento}`
-    vida1.innerText = `Hp: ${monstro1.vida}`
-    dano1.innerText = `Dano: ${monstro1.dano}`
-    energia1.innerText = `Energia: ${monstro1.energia}`
-    imagem1.setAttribute('src', `${monstro1.src}`)
-
-// Monstro inimigo
-    let nome2 = document.getElementById('nome2')
-    let elemento2 = document.getElementById('elemento2')
-    let vida2 = document.getElementById('vida2')
-    let dano2 = document.getElementById('dano2')
-    let energia2 = document.getElementById('energia2')
-    let imagem2 = document.getElementById('imagem2')
-
-    nome2.innerText = `${monstro2.nome}`
-    elemento2.innerText = `Elemento: ${monstro2.elemento}`
-    vida2.innerText = `Hp: ${monstro2.vida}`
-    dano2.innerText = `Dano: ${monstro2.dano}`
-    energia2.innerText = `Energia: ${monstro2.energia}`
-    imagem2.setAttribute('src', `${monstro2.src}`)
-}
-
-let monstro1 = {
-    nome:'Serpendragon',elemento:'Natureza',vida:4000,dano:1200,energia:20,src:'../midia/serpendragon.jpg'}
-
-let monstro2 = {
-    nome:'Colossus', elemento:'água', vida:7800,dano:1000, energia:20, src:'../midia/colossus.jpg'}
-
 let c1 = 1
-let c2 = 1
+let c2 = 2
 let inicio = 1
 let fim = 3
 let nome = [
@@ -108,10 +60,20 @@ function TrocarMonstro(monstro, sentido){
     }
     else if (monstro == 2){
         if (sentido == 'esquerda'){
-            
+            if (c2 == inicio){
+                alert('já está no primeiro')
+            }
+            else{
+                c2--
+            } 
         }
         else{ //direita
-            
+            if (c2 == fim){
+                alert('Já chegou no último')
+            }
+            else{
+                c2++
+            }
         }
     }
     else{
@@ -121,4 +83,44 @@ function TrocarMonstro(monstro, sentido){
     Batalha()
 }
 
-// TROCAR INDICADOR DO MONSTRO1 E 2 PELO INDICADOR C
+
+
+
+function Batalha(){
+
+// Monstro seleconado
+    let nome1 = document.getElementById('nome1')
+    let elemento1 = document.getElementById('elemento1')
+    let vida1 = document.getElementById('vida1')
+    let dano1 = document.getElementById('dano1')
+    let energia1 = document.getElementById('energia1')
+    let imagem1 = document.getElementById('imagem1')
+
+    nome1.innerText = `${nome[c1]}`
+    elemento1.innerText = `Elemento: ${elemento[c1]}`
+    vida1.innerText = `Hp: ${vida[c1]}`
+    dano1.innerText = `Dano: ${dano[c1]}`
+    energia1.innerText = `Energia: ${energia[c1]}`
+    imagem1.setAttribute('src', `${src[c1]}`)
+
+// Monstro inimigo
+    let nome2 = document.getElementById('nome2')
+    let elemento2 = document.getElementById('elemento2')
+    let vida2 = document.getElementById('vida2')
+    let dano2 = document.getElementById('dano2')
+    let energia2 = document.getElementById('energia2')
+    let imagem2 = document.getElementById('imagem2')
+
+    nome2.innerText = `${nome[c2]}`
+    elemento2.innerText = `Elemento: ${elemento[c2]}`
+    vida2.innerText = `Hp: ${vida[c2]}`
+    dano2.innerText = `Dano: ${dano[c2]}`
+    energia2.innerText = `Energia: ${energia[c2]}`
+    imagem2.setAttribute('src', `${src[c2]}`)
+}
+
+
+function selecionar(){
+    alert('selecionado')
+}
+
