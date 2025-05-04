a4b3e95d5ab9c8f98a27616f85f92dab78aa5844
Guilherme Alberto <gmightasf@gmail.com>
2025-05-03 21:17:35 -0300

    update atk and def

--- script/batalha.js
+++ script/batalha.js
@@ -80,13 +80,13 @@ function TrocarMonstro(monstro, sentido){
         alert('Houve um problema no processamento de dados <br> Entre em contato com um desenvolvedor e relate o problema')
     }
 
-    Batalha()
+    exibir()
 }
 
 
 
 
-function Batalha(){
+function exibir(){
 
 // Monstro seleconado
     let nome1 = document.getElementById('nome1')
@@ -121,6 +121,107 @@ function Batalha(){
 
 
 function selecionar(){
-    alert('selecionado')
+    sessionStorage.setItem('c1', c1)
+    sessionStorage.setItem('c2', c2)
+    //alert(`Valor do c1 é ${c1} e c2 é ${c2}`)
+
 }
+
+function processarC(){
+    c1 = parseInt(sessionStorage.getItem('c1'))
+    c2 = parseInt(sessionStorage.getItem('c2'))
+    //alert(`Valor do c1 é ${c1} e c2 é ${c2}`)    
+    exibir()
+}
+
+function atacar(){
+    if(vida[c2] > 0 && vida[c1] > 0 && vida[c2] - dano[c1] > 0 && vida[c1] - dano[c2] > 0){
+        acaoInimigo()
+    }
+    else if(vida[c2] - dano[c1] <= 0){ // Vitória
+        alert(`Você VENCEU! Muito bem`)
+        src[c2] = `../midia/vazio.png`
+        vida[c2] = 0
+    }
+    else if(vida[c1] - dano[c2] <= 0){ // Derrota
+        alert(`Você PERDEU! Tente novamente`)
+        src[c1] = `../midia/vazio.png`
+        vida[c1] = 0
+    }
+    else{ // ERRO ou INVÁLIDO
+        alert("ERROR")
+    }
+    exibir() 
+}
+
+function defender(){
+    let possibilidades = ['Defesa', 'MeiaDefesa', 'ContraGolpe']
+    let resultado = possibilidades[Math.floor(Math.random()*possibilidades.length)]
+    
+    if(resultado == 'Defesa'){
+        alert(`${nome[c1]} DEFENDEU o ataque`)
+    }
+    else if(resultado == 'MeiaDefesa'){
+        vida[c1] -= dano[c2] / 2
+        alert(`${nome[c1]} DEFENDEU PARTE do ataque, recebendo ${dano[c2]/2} de dano e ficando com ${vida[c1]}`)
+    }
+    else{
+        vida[c2] -= dano[c1] / 3
+        alert(`${nome[c1]} INTERCEPTOU o ataque de ${nome[c2]}, causando ${dano[c1] / 3} de DANO a ele`)
+        vida[c2] -= dano[c1]
+        alert(`Depois disso atacou novamente, CAUSANDO ${dano[c1]} e deixando ${nome[c2]} com ${vida[c2]}`)
+    }
+    exibir()
+}
+
+function recarregar(){
+    alert(`Esta função está em desenvolvimento.`)
+    exibir()
+}
+
+function acaoInimigo(){
+    let possibilidades = ['Atacar', 'Defender']
+    let resultado = possibilidades[Math.floor(Math.random()*possibilidades.length)]
+    if(resultado == 'Atacar'){
+        vida[c2] -= dano[c1]
+        alert(`${nome[c1]} ATACOU ${nome[c2]} deixando ele com ${vida[c2]} de vida!`)
+        vida[c1] -= dano[c2]
+        alert(`${nome[c2]} CONTRA-ATACOU deixando ele com ${vida[c1]} de vida!`)    
+    }
+    else{ //defender
+        let possibilidades = ['Defesa', 'MeiaDefesa', 'ContraGolpe']
+        let resultado = possibilidades[Math.floor(Math.random()*possibilidades.length)]
+
+        if(resultado == 'Defesa'){
+            alert(`${nome[c2]} DEFENDEU o ataque`)
+        }
+        else if(resultado == 'MeiaDefesa'){
+            vida[c2] -= dano[c1] / 2
+            alert(`${nome[c2]} DEFENDEU PARTE do ataque, recebendo ${dano[c1]/2} de dano e ficando com ${vida[c2]}`)
+        }
+        else{
+            vida[c1] -= dano[c2] / 3
+            alert(`${nome[c2]} INTERCEPTOU o ataque de ${nome[c1]}, causando ${dano[c2] / 3} de DANO a ele`)
+            vida[c1] -= dano[c2]
+            alert(`Depois disso atacou novamente, CAUSANDO ${dano[c2]} e deixando ${nome[c1]} com ${vida[c1]}`)
+        }
+        exibir()
+    }
+}
+
+
+
+/*
+function Batalhar(){
+    let meumonstro = {nome: nome[c1] , elemento: elemento[c1] , vida: vida[c1], dano: dano[c1], energia: energia[c1], imagem: src[c1]}
+
+    let inimigo = {nome: nome[c2] , elemento: elemento[c2] , vida: vida[c2], dano: dano[c2], energia: energia[c2], imagem: src[c2]}
+
+    function atacar(){
+        inimigo.vida -= meumonstro.dano
+        exibir()
+    }
+}
+*/
+
 
