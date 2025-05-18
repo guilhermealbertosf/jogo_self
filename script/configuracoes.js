b9e09f7937b34b7c55d8be7f8372c886786ffa6a
Guilherme Alberto <gmightasf@gmail.com>
2025-05-18 20:28:02 -0300

    update music

--- script/configuracoes.js
+++ script/configuracoes.js
@@ -6,20 +6,26 @@
     audio.currentTime = 10; // Muda para 10 segundos
     audio.volume = 0.5; // Define o volume para 50%
 */
-function musica(pagina){
+function musica(pagina=0){
     let musica_combate = document.getElementById("musicacombate")
-    if (pagina == 1){
-        musica_combate.currentTime = 0
-    }else if(pagina == 2){
-        musica_combate.currentTime = 60
-        musica_combate.volume = 0.2
-    }else if(pagina == 3){
-        musica_combate.currentTime = 48
-        musica_combate.volume = 0.6
+    if(pagina == 0){
+        musica_combate.pause()
+    }
+    else{
+        if (pagina == 1){
+            musica_combate.currentTime = 0
+        }else if(pagina == 2){
+            musica_combate.currentTime = 60
+            musica_combate.volume = 0.2
+        }else if(pagina == 3){
+            musica_combate.currentTime = 48
+            musica_combate.volume = 0.6
+        }
+        
+        
+        musica_combate.play()
     }
     
-    
-    musica_combate.play()
 }
 
 
